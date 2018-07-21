6f51a94bde23a76c73ae5dd4cd8b2e3f5ab76d44
Remon Adel <rad@ordbogen.com>
2018-07-21 16:16:40 +0200

    write all expected unit tests for converter class

--- tests/Converter/ConverterTest.php
+++ tests/Converter/ConverterTest.php
@@ -7,16 +7,85 @@ use PHPUnit\Framework\TestCase;
 
 class ConverterTest extends TestCase
 {
-    public function testSimpleConversion()
+    /**
+     * @param $input
+     * @param $output
+     * @dataProvider StringConversionProvider
+     */
+    public function testStringConversion($input, $output)
     {
-        $input = '{"key":"value","key2":"value2"}';
-        $output = [
-            'key' => 'value',
-            'key2' => 'value2'
-        ];
-
         $converter = new Converter();
 
         $this->assertEquals($output, $converter->convertString($input));
     }
+
+    public function StringConversionProvider()
+    {
+        return [
+            [
+                '{"key":"value","key2":"value2"}',
+                [
+                    'key'  => 'value',
+                    'key2' => 'value2',
+                ],
+            ],
+
+            [
+                '{"key":"value","key2":"value2","some-array":[1,2,3,4,5]}',
+                [
+                    'key'        => 'value',
+                    'key2'       => 'value2',
+                    'some-array' => [1, 2, 3, 4, 5],
+                ],
+            ],
+
+            [
+                '{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}',
+                [
+                    'key'        => 'value',
+                    'key2'       => 'value2',
+                    'some-array' => [1, 2, 3, 4, 5],
+                    'new-object' => [
+                        'key'  => 'value',
+                        'key2' => 'value2',
+                    ],
+                ],
+            ],
+
+            [
+                '[{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}},{"key":"value","key2":"value2","some-array":[1,2,3,4,5],"new-object":{"key":"value","key2":"value2"}}]',
+                [
+                    [
+                        'key'        => 'value',
+                        'key2'       => 'value2',
+                        'some-array' => [1, 2, 3, 4, 5],
+                        'new-object' => [
+                            'key'  => 'value',
+                            'key2' => 'value2',
+                        ],
+                    ],
+                    [
+                        'key'        => 'value',
+                        'key2'       => 'value2',
+                        'some-array' => [1, 2, 3, 4, 5],
+                        'new-object' => [
+                            'key'  => 'value',
+                            'key2' => 'value2',
+                        ],
+                    ],
+                    [
+                        'key'        => 'value',
+                        'key2'       => 'value2',
+                        'some-array' => [1, 2, 3, 4, 5],
+                        'new-object' => [
+                            'key'  => 'value',
+                            'key2' => 'value2',
+                        ],
+                    ],
+                ],
+            ],
+
+        ];
+    }
+
 }
